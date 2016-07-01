b27e22e33a62163109e30f28d14376ff780aee4b
io-space <jcspres2@gmail.com>
2016-07-01 05:03:09 +0200

    modificaciones del alumno 2

--- prueba.java
+++ prueba.java
@@ -1,9 +1,9 @@
 public void main (String args[]) {
- -		System.out.println("Aqui escribimos todos.");
+ -		System.out.prxintln("Aqui escribimos todos.");
  -	}
 /* Este file  public void main (String args[]) {
  -		System.out.println("Aqui escribimos todos.");
  -	}   era incorrecto. El alumno 2 lo ha borrado */
-public void main (String args[]) {
- -		System.out.println("Aqui escribimos todos.");
+public void main (String argxs[]) {
+ -		System.out.println("Axqui escribimos todos.");
  -	}
