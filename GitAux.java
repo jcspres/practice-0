23f2c10b72f889ee23e9fcf19f372e7f15c563c4
jcspres <jcspres@gmail.com>
2016-07-01 04:09:38 +0200

    El nuevo colaborador ha modificado GitAux.java

--- GitAux.java
+++ GitAux.java
@@ -1,12 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package prgitejemplo;
 
 /**
  *
- * @author bertoa
+ * @creator: jcspres
+ * @colaborador: io-space
  */
 public class GitAux {
     
