c494c69558910065a8bcf1cb03b682ee927f42b5
jcspres <jcspres2@gmail.com>
2016-07-01 06:50:21 +0200

    modificaciones alumno 2

--- GitAux.java
+++ GitAux.java
@@ -10,24 +10,11 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
-	System.out.println("insertado por alumno2"); //  modificado por alumno 2
+	System.out.println("modificado por alumno2");
     }
     
     public void metodoComunitario(){
-        int i = 0;
 	System.out.println("Aqui escribimos todos");
-	// modificaciones alumno 2
 	System.out.println("Alumno 2 inserta esta sentencia");
-	while(i>12) {
-	   System.out.println("Este bucle no sirve para nada.");
-	}
-    }
-    public void testGitAux(){
-	// metodo rama testing
-        System.out.println("Metodo de testing");
-    }
-    public void helper(){
-	// el alumno 2 ha creado este procedimiento
-	System.out.println("Hola, soy el alumno 2");
     }
 }
