209052c49ddb4028acb4b7bd8c16ec33751b7866
jcspres <jcspres@gmail.com>
2016-07-01 05:00:11 +0200

    nuevas modificaciones del alumno 1

--- prueba.java
+++ prueba.java
@@ -1,1 +1,9 @@
-/* Este file era incorrecto. El alumno 2 lo ha borrado */
+public void main (String args[]) {
+ -		System.out.println("Aqui escribimos todos.");
+ -	}
+/* Este file  public void main (String args[]) {
+ -		System.out.println("Aqui escribimos todos.");
+ -	}   era incorrecto. El alumno 2 lo ha borrado */
+public void main (String args[]) {
+ -		System.out.println("Aqui escribimos todos.");
+ -	}
